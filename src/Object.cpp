286a9eab9b12df40ec5c2a2f07b9c1cfb31f0868
onrdmr <onur.demir2@std.yildiz.edu.tr>
2023-04-30 17:00:22 +0300

    set json cıkarıldı

--- src/Object.cpp
+++ src/Object.cpp
@@ -41,9 +41,6 @@ Object::Object(const json& json):
     m_mbrRegion{ json["mbrRegion"] } {}
 
 
-void Object::setJson(json& json) { // there will be second parameter about schema in the future
-    std::cout << "json logic can be here" << std::endl;
-}
 
 
 std::string Object::id() { return m_oid; }
