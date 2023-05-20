a1d50868cd1aeff7cadca49381af06926aa1df1c
Melih Ocakcı <melihocaki@gmail.com>
2023-05-20 18:44:14 +0300

    switched to asynchronous sockets

--- src/examples/asio_test.cpp
+++ src/examples/asio_test.cpp
@@ -1,9 +1,10 @@
-#include <modb/Object.h>
 #include <modb/DatabaseResource.h>
 
-#include <db_cxx.h>
-#include <boost/asio.hpp>
+#include <boost/asio/placeholders.hpp>
+#include <boost/asio/ip/udp.hpp>
+#include <boost/asio/ip/basic_endpoint.hpp>
 #include <boost/array.hpp>
+#include <boost/bind/bind.hpp>
 
 #include <iostream>
 #include <string>
@@ -19,62 +20,101 @@
 using boost::asio::ip::udp;
 
 std::atomic<bool> exitProgram = false;
+boost::asio::io_context io_context;
 
 void SIGINT_handler(int param)
 {
     exitProgram = true;
+    io_context.stop();
 }
 
-void udpServer(std::shared_ptr<modb::DatabaseResource> db, unsigned short port) {
-    boost::asio::io_context io_context;
-    udp::endpoint endpoint{udp::v4(), port};
-    udp::socket socket{io_context, endpoint};
-
-    while (!exitProgram)
+class udp_server
+{
+public:
+    udp_server(boost::asio::io_context& io_context, unsigned short port, std::shared_ptr<modb::DatabaseResource> db) :
+        socket_(io_context, udp::endpoint(udp::v4(), port)),
+        db_(db)
     {
-        boost::array<char, 100> recv_buf;
-        udp::endpoint remote_endpoint;
-        boost::system::error_code ignored_error;
-
-        std::size_t len = socket.receive_from(boost::asio::buffer(recv_buf), remote_endpoint);
-        if (exitProgram) { break; }
-
-        std::string message{recv_buf.begin(), len};
-
-        int operation;
-        std::sscanf(message.c_str(), "%d ", &operation);
-
-        std::string reply{};
-
-        if (operation == 1) {
-            modb::Region queryRegion;
-            std::sscanf(message.c_str(), "%d %lf %lf %lf %lf",
-                &operation,
-                &queryRegion.pointLow().longitude(),
-                &queryRegion.pointLow().latitude(),
-                &queryRegion.pointHigh().longitude(),
-                &queryRegion.pointHigh().latitude()
-            );
-
-            std::vector<modb::Object> resultset = db->intersectionQuery(queryRegion);
-
-            std::for_each(resultset.begin(), resultset.end(),
-                [&](const modb::Object& object) {
-                    reply.append(object.id() + '\n');
-                }
-            );
-        }
-        else {
-            continue;
-        }
-
-        socket.send_to(boost::asio::buffer(reply), remote_endpoint, 0, ignored_error);
+        start_receive();
     }
 
-    std::cerr << "\nserver thread: exiting...";
+private:
+    void start_receive()
+    {
+        socket_.async_receive_from(
+            boost::asio::buffer(recv_buffer_),
+            remote_endpoint_,
+            boost::bind(&udp_server::handle_receive,
+                this,
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
+    }
+
+    void handle_receive(const boost::system::error_code& error,
+        std::size_t bytes_transferred)
+    {
+        if (error) {
+            return;
+        }
+
+        std::string message{recv_buffer_.begin(), bytes_transferred};
+
+        // int operation;
+        // std::sscanf(message.c_str(), "%d ", &operation);
+
+        auto reply = std::make_shared<std::string>();
+
+        // if (operation == 1) {
+        modb::Region queryRegion;
+        std::sscanf(message.c_str(), "%lf %lf %lf %lf",
+            &queryRegion.pointLow().longitude(),
+            &queryRegion.pointLow().latitude(),
+            &queryRegion.pointHigh().longitude(),
+            &queryRegion.pointHigh().latitude()
+        );
+
+        std::vector<modb::Object> resultset = db_->intersectionQuery(queryRegion);
+
+        std::for_each(resultset.begin(), resultset.end(),
+            [&](const modb::Object& object) {
+                reply->append(object.id() + '\n');
+            }
+        );
+
+        if (reply->empty()) {
+            reply->append("\n");
+        }
+
+        socket_.async_send_to(boost::asio::buffer(*(reply.get())),
+            remote_endpoint_,
+            boost::bind(&udp_server::handle_send, this,
+                boost::asio::placeholders::error));
+
+        // }
+
+        start_receive();
+    }
+
+    void handle_send(const boost::system::error_code& error)
+    {
+        if (error) {
+            std::cerr << "error\n";
+            SIGINT_handler(0);
+        }
+    }
+
+    udp::socket socket_;
+    udp::endpoint remote_endpoint_;
+    boost::array<char, 100> recv_buffer_;
+    std::shared_ptr<modb::DatabaseResource> db_;
+};
+
+void udpServer(std::shared_ptr<modb::DatabaseResource> db, unsigned short port) {
+    udp_server server(io_context, port, db);
+    io_context.run();
 }
 
-void loadData(std::shared_ptr<modb::DatabaseResource> db) {
+void dataLoader(std::shared_ptr<modb::DatabaseResource> db) {
     std::string line;
 
     while (!exitProgram)
@@ -82,7 +122,7 @@ void loadData(std::shared_ptr<modb::DatabaseResource> db) {
         std::getline(std::cin, line);
 
         if (line.empty()) {
-            std::cerr << "waiting..." << std::endl;
+            std::cerr << "modb: waiting..." << std::endl;
             sleep(1);
             continue;
         }
@@ -92,8 +132,6 @@ void loadData(std::shared_ptr<modb::DatabaseResource> db) {
 
         db->putObject(parsedObject);
     }
-
-    std::cerr << "\nloader thread: exiting...";
 }
 
 int main(int argc, char** argv) {
@@ -107,7 +145,7 @@ int main(int argc, char** argv) {
     try {
         const auto db = std::make_shared<modb::DatabaseResource>(std::string(argv[1]), DB_BTREE, DB_CREATE);
 
-        std::thread loaderThread{loadData, db};
+        std::thread loaderThread{dataLoader, db};
         std::thread serverThread{udpServer, db, static_cast<unsigned short>(std::stoi(argv[2]))};
 
         loaderThread.join();
